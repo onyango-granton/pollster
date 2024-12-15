3c510add97492c5fadd544fd7daa0d9c5eea3538
gonyango <grantononyango@gmail.com>
2024-12-15 21:26:57 +0300

    feat: add choice class

--- pollster/polls/models.py
+++ pollster/polls/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Questions(models.Model): #extends model
+class Question(models.Model): #extends model
     #id automatically created
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
