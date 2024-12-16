f5c673d262f882460b2369795bbbe09ccfea1f36
gonyango <grantononyango@gmail.com>
2024-12-16 03:57:44 +0300

    feat detail function tat fetches and returns specific question and choice and renders it onto polls/result.html

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -10,3 +10,11 @@ def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list':latest_question_list}
     return render(request, 'polls/index.html', context)
+
+# Get specific question and choices
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question Does not exsist")
+    return render(request, "polls/results.html", {'question':question})
