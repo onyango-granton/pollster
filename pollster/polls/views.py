09076d39389d170c50a2bf62f9455257fd9b723e
gonyango <grantononyango@gmail.com>
2024-12-16 06:08:22 +0300

    added votr function to polls

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 
-from django.http import Http404
+from django.http import Http404, HttpResponseRedirect
 
 from django.shortcuts import get_object_or_404
+
+from django.urls import reverse
 
 # Create your views here.
 from .models import Question, Choice
@@ -25,3 +27,18 @@ def detail(request, question_id):
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html', {'question':question})
+
+#vote for a question
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except:
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message':"You didnt select a choice"
+        })
+    else:
+        selected_choice.votes += 1 
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
